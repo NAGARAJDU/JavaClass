37a24d999729e26ad61069de2d0a1da3ba614017
NAGARAJ <nagarajumatar@gmail.com>
2025-08-03 16:04:02 +0530

    first commit

---
+++ Bitwise.java
@@ -0,0 +1,17 @@
+public class Bitwise {
+
+    public class Bitwise {
+
+        public static void main(String[] args)
+
+        byte c;
+         
+        c=(byte)(9<<4);
+        c=(byte)(c|12);
+
+        System.out.println((c&0b11110000)>>4);
+        System.out.println((c&0b00001111)>>4);
+    
+    }
+    
+}
